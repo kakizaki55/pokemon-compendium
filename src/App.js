010022837a58820fa0be0c1bc2d0232dc35506f4
minoka kakizaki <minoka.kakizaki@gmail.com>
2021-12-08 12:44:42 -0800

    got the seting of the pokemon list

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import { useState, useEffect } from 'react';
 import fetchPokemon from './services/pokemon';
 
 function App() {
+  const [pokemon, setPokemon] = useState();
+
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetchPokemon();
-      console.log(data.results);
+      setPokemon(data.results);
     };
     fetchData();
   }, []);
+
+  console.log(pokemon);
 
   return <div className="App">Pokemon App</div>;
 }
