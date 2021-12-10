01dab529d304f1d685e42e026fc61d3a3527868e
minoka kakizaki <minoka.kakizaki@gmail.com>
2021-12-10 15:50:19 -0800

    taking abreak

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,5 +2,5 @@ import React from 'react';
 import './Footer.css';
 
 export default function Footer() {
-  return <footer>made my minoka</footer>;
+  return <footer>made my minoka shout out to, Bailey, Spencer, Zack and Karyssa!!</footer>;
 }
--- src/components/PokemonCard/PokemonCard.js
+++ src/components/PokemonCard/PokemonCard.js
@@ -5,6 +5,7 @@ export default function PokemonCard({
   pokemon,
   url_image,
   type_1,
+  type_2,
   hp,
   height,
   weight,
@@ -15,33 +16,39 @@ export default function PokemonCard({
 }) {
   return (
     <div className={`pokemon-card , ${type_1}`}>
-      <div className="line-1">
-        <span id="name">{pokemon}</span>
-        <div>
-          <span id="hp">{hp} HP</span>
-          <img src={process.env.PUBLIC_URL + `/assests/${type_1}-attack.webp`} />
+      <div className={`${type_2}op`}>
+        <div className="line-1">
+          <span id="name">{pokemon}</span>
+          <div>
+            <span id="hp">{hp} HP</span>
+            <img id="power-elem" src={process.env.PUBLIC_URL + `/assests/${type_1}-attack.webp`} />
+          </div>
         </div>
-      </div>
-      <img src={`${url_image}`}></img>
-      <div id="info-panel">{`${type_1} Pokemon. Length: ${height}', Weight:${weight} lbs.`}</div>
-      <div className="moves">
-        <div id="move-1">
-          <h6>{ability_1}</h6>
-          <h6>{attack}</h6>
+        <img src={`${url_image}`}></img>
+        <div id="info-panel">{`${type_1} Pokemon. Length: ${height}', Weight:${weight} lbs.`}</div>
+        <div className="moves">
+          <div id="move-1">
+            <h6>{ability_1}</h6>
+            <h6>{attack}</h6>
+          </div>
+          {ability_2 !== 'NA' && (
+            <>
+              <hr />
+              <div id="move-2">
+                <h6>{ability_2}</h6>
+                <h6>{special_attack}</h6>
+              </div>
+            </>
+          )}
         </div>
-        {ability_2 !== 'NA' && (
-          <>
-            <hr />
-            <div id="move-2">
-              <h6>{ability_2}</h6>
-              <h6>{special_attack}</h6>
-            </div>
-          </>
-        )}
-      </div>
-      <div>
-        <hr />
-        alsdkfhjalsdf
+        <footer className="bottom-bit">
+          <hr />
+          <div className="bottomer-bit">
+            <div>weakness</div>
+            <div>resistance</div>
+            <div>retreat cost</div>
+          </div>
+        </footer>
       </div>
     </div>
   );
